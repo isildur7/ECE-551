479853b8e0db0dcefb92410909483080309217bf
aac55 <aac55@duke.edu>
2018-09-05 00:22:10 -0400

    012-0

---
+++ 012_complex_ty/mytypes.c
@@ -0,0 +1,8 @@
+struct complex_num_tag {
+  double real;
+  double imag;
+};
+typedef struct complex_num_tag complex_num_t;
+
+enum animal_tag { CAT, FISH, SNAKE, DOG, HORSE };
+typedef enum animal_tag animal_t;
