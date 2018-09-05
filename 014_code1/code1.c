5e9b7597abc69750bec8556cdc7eb8094516398f
aac55 <aac55@duke.edu>
2018-09-05 11:35:19 -0400

    014-0

--- 014_code1/code1.c
+++ 014_code1/code1.c
@@ -1,7 +1,8 @@
-int max (int num1, int num2) {
+int max(int num1, int num2) {
   //check if num1 is greater than num2
-    //if so, your answer is num1
-    //otherwise, your answer is num2
+  //if so, your answer is num1
+  //otherwise, your answer is num2
+  return num1 > num2 ? num1 : num2;
 }
 
 int main(void) {
@@ -9,8 +10,6 @@ int main(void) {
   printf("max(33, 0) is %d\n", max(33, 0));
   printf("max(0x123456, 123456) is %d\n", max(0x123456, 123456));
   //print the max of 0x451215AF and 0x913591AF and prints it out as a decimal number
-  
+  printf("max(0x451215AF,0x913591AF ) is %d\n", max((int)0x9131215af, (int)0x451215af));
   return 0;
 }
-
-
