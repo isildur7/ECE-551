62f5bbd2e5d4ca734955551dde84f0a0c2678755
aac55 <aac55@duke.edu>
2018-09-08 17:26:41 -0400

    020-0

--- 020_rect/rectangle.c
+++ 020_rect/rectangle.c
@@ -17,14 +17,56 @@ int max(int a, int b) {
 }
 
 // Declare your rectangle structure here!
+typedef struct rectangle_t {
+  int x;
+  int y;
+  int height;
+  int width;
+} rectangle_t;
 
 rectangle_t canonicalize(rectangle_t r) {
   // WRITE THIS FUNCTION
+  if (r.width < 0) {
+    r.x = r.x + r.width;
+    r.width -= 2 * r.width;
+  }
+  if (r.height < 0) {
+    r.y = r.y + r.height;
+    r.height -= 2 * r.height;
+  }
   return r;
 }
+
 rectangle_t intersection(rectangle_t r1, rectangle_t r2) {
   // WRITE THIS FUNCTION
-  return r1;
+  rectangle_t inter;
+
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+
+  inter.y = max(r1.y, r2.y);
+  inter.x = max(r1.x, r2.x);
+
+  if ((((min(r2.y, r1.y) == r2.y) && (r1.y - r2.y > r2.height)) ||
+       ((min(r2.y, r1.y) == r1.y) && (r2.y - r1.y > r1.height))) ||
+      (((min(r2.x, r1.x) == r1.x) && (r2.x - r1.x > r1.width)) ||
+       ((min(r2.x, r1.x) == r2.x) && (r1.x - r2.x > r2.width)))) {
+    inter.width = 0;
+    inter.height = 0;
+  }
+  /*  else {
+    if (((min(r2.y, r1.y) == r2.y) && (r1.y - r2.y == r2.height)) ||
+        ((min(r2.y, r1.y) == r1.y) && (r2.y - r1.y == r1.height)))
+      inter.height = 0;
+    else if (((min(r2.x, r1.x) == r1.x) && (r2.x - r1.x > r1.width)) ||
+             ((min(r2.x, r1.x) == r2.x) && (r1.y - r2.x > r2.width)))
+	     inter.width = 0;*/
+  else {
+    inter.width = min(r1.x + r1.width, r2.x + r2.width) - inter.x;
+    inter.height = min(r1.y + r1.height, r2.y + r2.height) - inter.y;
+  }
+  // }
+  return inter;
 }
 
 // You should not need to modify any code below this line
@@ -32,7 +74,8 @@ void printRectangle(rectangle_t r) {
   r = canonicalize(r);
   if (r.width == 0 && r.height == 0) {
     printf("<empty>\n");
-  } else {
+  }
+  else {
     printf("(%d,%d) to (%d,%d)\n", r.x, r.y, r.x + r.width, r.y + r.height);
   }
 }
