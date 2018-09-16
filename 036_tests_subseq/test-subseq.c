1af875a5b61ba92678c57b71e81e64f2211626a0
aac55 <aac55@duke.edu>
2018-09-16 19:45:07 -0400

    changed previous commit for grading

--- 036_tests_subseq/test-subseq.c
+++ 036_tests_subseq/test-subseq.c
@@ -16,13 +16,13 @@ int main() {
   int arr1[] = {1, 2, 3, 4, 5, 5, 5, 5, 8, 9, 87, 5674};  //12
   int arr3[] = {-1, -7, -4, 0, 4, 7, 4};                  //7
   int arr4[] = {0};
-  int arr5[] = {};
+  //  int arr5[] = {};
   int arr6[] = {0, -4, -6, -8, -7};
 
   testSeq(arr1, 12, 5);
   testSeq(arr3, 7, 5);
   testSeq(arr4, 1, 1);
-  testSeq(arr5, 0, 0);
+  //  testSeq(arr5, 0, 0);
   testSeq(arr6, 5, 2);
 
   return EXIT_SUCCESS;
