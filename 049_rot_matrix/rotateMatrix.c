e1219e9a9950d3026e836593a725cfbb078a15a0
aac55 <aac55@duke.edu>
2018-09-25 22:49:00 -0400

    049-0

---
+++ 049_rot_matrix/rotateMatrix.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_SIZE 10
+
+void rot_matrix(FILE * f) {
+  int i = 0;
+  char array[MAX_SIZE][MAX_SIZE + 2];
+  while (fgets(array[i], MAX_SIZE + 2, f) != NULL) {
+    if (i >= MAX_SIZE) {
+      fprintf(stderr, "more than 10 lines\n");
+      exit(EXIT_FAILURE);
+    }
+    int yank = strchr(array[i], '\n') - array[i];
+    if (yank != 10) {
+      fprintf(stderr, "line too long\n");
+      exit(EXIT_FAILURE);
+    }
+    i++;
+  }
+  char rotation[10][11];
+  for (int old_i = 0; old_i < MAX_SIZE; old_i++) {
+    for (int old_j = 0; old_j < MAX_SIZE; old_j++) {
+      int new_i = old_j;
+      int new_j = MAX_SIZE - old_i - 1;
+      rotation[new_i][new_j] = array[old_i][old_j];
+    }
+    rotation[old_i][MAX_SIZE] = '\0';
+  }
+  for (i = 0; i < MAX_SIZE; i++) {
+    fprintf(stdout, "%s\n", rotation[i]);
+  }
+}
+
+int main(int argc, char ** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: rotateMatrix inputFileName\n");
+    return EXIT_FAILURE;
+  }
+  FILE * f = fopen(argv[1], "r");
+  if (f == NULL) {
+    perror("Could not open file");
+    return EXIT_FAILURE;
+  }
+  rot_matrix(f);
+  if (fclose(f) != 0) {
+    perror("Failed to close the input file!");
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
