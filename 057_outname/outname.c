2395cb8feb9d25691f608f9ee1f3edab63fdb9fe
aac55 <aac55@duke.edu>
2018-10-01 00:59:26 -0400

    057-0

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,27 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  if (inputName == NULL) {
+    return NULL;
+  }
+
+  const char * addition = ".counts";
+  size_t length = strlen(inputName) + 8;
+
+  char * output = malloc(length * sizeof(*output));
+  if (output == NULL) {
+    fprintf(stderr, "Not enough memory\n");
+    exit(EXIT_FAILURE);
+  }
+
+  for (size_t i = 0; i <= strlen(inputName); i++) {
+    output[i] = inputName[i];
+  }
+  strcat(output, addition);
+  return output;
 }
