d11179fedb4b8c2d42911cdb8a09c8011ba8387c
aac55 <aac55@duke.edu>
2018-10-17 16:48:55 -0400

    072-0

--- 072_int_array/IntArray.cpp
+++ 072_int_array/IntArray.cpp
@@ -1,37 +1,84 @@
 #include "IntArray.h"
+
 #include <assert.h>
+#include <string.h>
+
+#include <cstdio>
 #include <ostream>
 
-IntArray::IntArray() {
+IntArray::IntArray() : data(NULL), numElements(0) {}
 
-}
-IntArray::IntArray(int n) {
+IntArray::IntArray(int n) : data(new int[n]), numElements(n) {}
 
-}
-
-IntArray::IntArray(const IntArray & rhs)  {
-
+IntArray::IntArray(const IntArray & rhs) :
+    data(new int[rhs.numElements]),
+    numElements(rhs.numElements) {
+  for (int i = 0; i < numElements; i++) {
+    data[i] = rhs.data[i];
+  }
 }
 IntArray::~IntArray() {
-
+  delete[] data;
 }
 
 IntArray & IntArray::operator=(const IntArray & rhs) {
+  if (this != &rhs) {
+    int * temp = new int[rhs.numElements];
+    for (int i = 0; i < rhs.numElements; i++) {
+      temp[i] = rhs.data[i];
+    }
+    delete[] data;
+    data = temp;
+    numElements = rhs.numElements;
+  }
+  return *this;
 }
+
 const int & IntArray::operator[](int index) const {
+  assert(index < numElements && index >= 0);
+  const int & ref = data[index];
+  return ref;
 }
+
 int & IntArray::operator[](int index) {
+  assert(index < numElements && index >= 0);
+  int & ref = data[index];
+  return ref;
 }
 
 int IntArray::size() const {
+  return numElements;
 }
 
 bool IntArray::operator==(const IntArray & rhs) const {
+  if (numElements != rhs.numElements) {
+    return false;
+  }
+  for (int i = 0; i < numElements; i++) {
+    if (data[i] != rhs.data[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 bool IntArray::operator!=(const IntArray & rhs) const {
+  if (*this == rhs) {
+    return false;
+  }
+  return true;
 }
 
 std::ostream & operator<<(std::ostream & s, const IntArray & rhs) {
-
+  s.write("{", 1);
+  for (int i = 0; i < rhs.size(); i++) {
+    char asString[sizeof(rhs[i])];
+    snprintf(asString, sizeof(rhs[i]), "%d", rhs[i]);
+    s.write(asString, strlen(asString));
+    if (i != rhs.size() - 1) {
+      s.write(", ", 2);
+    }
+  }
+  s.write("}", 1);
+  return s;
 }
