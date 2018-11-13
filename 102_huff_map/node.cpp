aec4820fb455c4a3d4c9704c0644c2d6149be6ee
aac55 <aac55@duke.edu>
2018-11-13 16:41:03 -0500

    102-0

--- 102_huff_map/node.cpp
+++ 102_huff_map/node.cpp
@@ -1,5 +1,13 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
   //WRITE ME!
+  if (sym != NO_SYM) {
+    assert(right == NULL && left == NULL);
+    theMap.insert(std::pair<unsigned, BitString>(sym, b));
+  }
+  else {
+    assert(right != NULL && left != NULL);
+    left->buildMap(b.plusZero(), theMap);
+    right->buildMap(b.plusOne(), theMap);
+  }
 }
-
