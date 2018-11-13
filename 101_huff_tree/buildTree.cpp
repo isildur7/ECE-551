d05f50fe0364c26c47166e04d4e751b92eced08d
aac55 <aac55@duke.edu>
2018-11-13 15:14:30 -0500

    101-0

--- 101_huff_tree/buildTree.cpp
+++ 101_huff_tree/buildTree.cpp
@@ -1,5 +1,22 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+  for (unsigned i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  while (pq.size() > 1) {
+    Node * left = pq.top();
+    pq.pop();
+    Node * right = pq.top();
+    pq.pop();
+    Node * tree = new Node(left, right);
+    pq.push(tree);
+  }
+  Node * tree = pq.top();
+  pq.pop();
+  return tree;
+}
